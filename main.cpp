255dbb6ed1f9ebe1b0202a95dd2c896571675324
Dejan <673008865@qq.com>
2018-10-11 22:41:42 +0800

    Dejan

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "SimpleRedis.h"
+using namespace std;
+
+int main()
+{
+	try{
+		//SimpleRedis *rd = new SimpleRedis();
+		//SimpleRedis *rd = new SimpleRedis("127.0.0.1", 6379);
+
+		struct timeval timeout = { 1, 500000 }; // 1.5 seconds
+		SimpleRedis *rd = new SimpleRedis("127.0.0.1", 6379, timeout);
+
+		redisReply* reply = NULL;
+
+		/* PING server */
+		reply = rd->exec("PING");
+		cout<<reply->str<<endl;
+
+		/* Set a key */
+		reply = rd->exec("SET %s %s", "foo", "helloword!");
+		cout<<reply->str<<endl;
+		
+		
+		// 释放内存
+		rd->destroy();
+		reply = NULL;
+	}
+	catch(const exception &e)
+	{
+		cerr<<"Get exception : "<<e.what()<<endl;
+	}
+
+
+	return 0;
+}
+
+// g++ main.cpp SimpleRedis.cpp -I ./ -I /usr/local/include/hiredis/ -l hiredis -std=c++11
+
